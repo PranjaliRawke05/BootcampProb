6b24b9ef729b5be0ef9cb97abfc45b545d412d55
PranjaliRawke05 <Pranjaliprawke@gmail.com>
2021-03-03 16:29:59 -0800

    Print Week Days

---
+++ Case/02WeekDay.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Enter number:"
+read num
+
+case $num in
+	1)echo "Sunday" ;;
+	2)echo "Monday" ;;
+	3)echo "Tuesday" ;;
+	4)echo "Wednesday" ;;
+	5)echo "Thersday" ;;
+	6)echo "Friday" ;;
+	7)echo "Saturday" ;;
+esac
