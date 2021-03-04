85bba0bb074d9dd52acfdeab881fc7ec234ae5b0
PranjaliRawke05 <Pranjaliprawke@gmail.com>
2021-03-03 16:37:49 -0800

    Display Week Days

---
+++ IFElif/02WeekDay.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+echo "Enter number:"
+read num
+
+if [ $num -eq 0 ]
+then
+        echo "Sunday"
+elif [ $num -eq 1 ]
+then
+        echo "Monday"
+elif [ $num -eq 2 ]
+then
+        echo "Tuesday"
+elif [ $num -eq 3 ]
+then
+        echo "Wednesday"
+elif [ $num -eq 4 ]
+then
+        echo "Thursday"
+elif [ $num -eq 5 ]
+then
+        echo "Friday"
+elif [ $num -eq 6 ]
+then
+        echo "Saturday"
+else
+        echo "Number is not between 0-6"
+fi
