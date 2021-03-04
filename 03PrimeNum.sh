2e23ae4928a810468b49b56d4345e187a0ee09c3
PranjaliRawke05 <Pranjaliprawke@gmail.com>
2021-03-03 16:56:42 -0800

    Print Prime Number

---
+++ 03PrimeNum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+echo "Enter Number"
+read num
+
+for (( i=2; i<=$num/2; i++))
+do
+	if [ $(($num%$i)) -eq 0 ]
+	then
+		echo $num"is not a prime number"
+	fi
+
+done
+echo $num"is a prime numebr"
+
